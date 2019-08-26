3148482df41860ad75ed14198432d8d336ca1822
Gus Mueller <gus@asecular.com>
2019-08-26 17:40:09 -0400

    better left and right

--- boatleft.py
+++ boatleft.py
@@ -3,9 +3,9 @@ import  RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24,GPIO.OUT)
-GPIO.output(24, GPIO.HIGH)
+GPIO.output(24, True)
 
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(12,GPIO.OUT)
-GPIO.output(13, GPIO.LOW)
-GPIO.output(12, GPIO.HIGH)
+GPIO.output(13, False)
+GPIO.output(12, True)
--- boatright.py
+++ boatright.py
@@ -3,12 +3,8 @@ import  RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24,GPIO.OUT)
-GPIO.output(24, GPIO.LOW)
-#GPIO.setup(23, GPIO.OUT)
-#GPIO.output(23, GPIO.HIGH)
-
-
+GPIO.output(24, True)
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(12,GPIO.OUT)
-GPIO.output(13, GPIO.LOW)
-GPIO.output(12, GPIO.HIGH)
+GPIO.output(13, True)
+GPIO.output(12, False)
